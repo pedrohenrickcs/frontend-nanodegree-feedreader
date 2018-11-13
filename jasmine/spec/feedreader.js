c2b689988272d51ff71af34f99915726f7d8c3f6
unknown <pedro.henrickcs@gmail.com>
2018-11-12 23:20:29 -0200

    desenvolvimento

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,48 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('Validate URL defined but not empty', function () {
+            for (const all in allFeeds) {
+                if (allFeeds.hasOwnProperty(all)) {
+                    const element = allFeeds[all].url;                    
+                    expect(element).toBeTruthy();
+                }
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('Validate name defined but not empty', function () {
+            for (const all in allFeeds) {
+                if (allFeeds.hasOwnProperty(all)) {
+                    const element = allFeeds[all].name;
+                    expect(element).toBeTruthy();
+                }
+            }
+        });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('Validate if the menu item is hidden', function () {
+           expect($('body').toHaveClass('menu-hidden'));
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -69,4 +89,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    })
 }());
